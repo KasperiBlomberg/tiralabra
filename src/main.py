bcfb339ed86ca4a1f1cf76762539c5f2ad055ab3
KasperiBlomberg <blomberg.kasperi@gmail.com>
2025-02-01 20:11:06 +0200

    Create basic console UI

--- src/main.py
+++ src/main.py
@@ -1,95 +1,26 @@
-import utils.mnist_reader as mnist_reader
-import numpy as np
+from scripts.train import train
+from scripts.evaluate import evaluate
 
-import os
+def main():
+    while True:
+        print("1. Train the model")
+        print("2. Evaluate the model")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
-DATA_DIR = os.path.join(BASE_DIR, 'data/fashion')
+        if choice == "1":
+            sample_size = int(input("Enter the sample size: (max 60000)"))
+            iterations = int(input("Enter the number of iterations: "))
+            alpha = float(input("Enter the learning rate: "))
+            train(alpha, iterations, sample_size)
+        elif choice == "2":
+            sample_size = int(input("Enter the sample size: (max 10000)"))
+            evaluate(sample_size)
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice. Please enter a valid choice.")
 
-X_train, y_train = mnist_reader.load_mnist(DATA_DIR, kind='train')
-X_test, y_test = mnist_reader.load_mnist(DATA_DIR, kind='t10k')
 
-X_train = X_train[:10000]
-y_train = y_train[:10000]
-X_train = X_train.T
-y_train = y_train.T
-X_train = X_train / 255
-
-def one_hot(Y):
-    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
-    one_hot_Y[np.arange(Y.size), Y] = 1
-    one_hot_Y = one_hot_Y.T
-    return one_hot_Y
-
-def init_params():
-    W1 = np.random.rand(10, 784) - 0.5
-    b1 = np.random.rand(10, 1) - 0.5
-    W2 = np.random.rand(10, 10) - 0.5
-    b2 = np.random.rand(10, 1) - 0.5
-    return W1, b1, W2, b2
-
-def sigmoid(x):
-    """x is the activation value of a neuron."""
-    return 1 / (1 + np.exp(-x))
-
-def sigmoid_derivative(x):
-    """Compute the derivative of the sigmoid function."""
-    return x * (1 - x)
-
-def ReLU(Z):
-    return np.maximum(Z, 0)
-
-def ReLU_deriv(Z):
-    return Z > 0
-
-def softmax(Z):
-    A = np.exp(Z) / sum(np.exp(Z))
-    return A
-
-def forward_prop(W1, b1, W2, b2, X):
-    Z1 = W1.dot(X) + b1
-    A1 = ReLU(Z1)
-    Z2 = W2.dot(A1) + b2
-    A2 = softmax(Z2)
-    return Z1, A1, Z2, A2
-
-def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
-    one_hot_Y = one_hot(Y)
-    m = Y.size
-    dZ2 = A2 - one_hot_Y
-    dW2 = 1 / m * dZ2.dot(A1.T)
-    db2 = 1 / m * np.sum(dZ2)
-    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)   # for sigmoid dZ1 = W2.T.dot(dZ2) * sigmoid_derivative(A1)
-    dW1 = 1 / m * dZ1.dot(X.T)
-    db1 = 1 / m * np.sum(dZ1)
-    return dW1, db1, dW2, db2
-
-def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
-    W1 = W1 - alpha * dW1
-    b1 = b1 - alpha * db1    
-    W2 = W2 - alpha * dW2  
-    b2 = b2 - alpha * db2    
-    return W1, b1, W2, b2
-
-def get_predictions(A2):
-    return np.argmax(A2, 0)
-
-def get_accuracy(predictions, Y):
-    return np.sum(predictions == Y) / Y.size
-
-def gradient_descent(X, Y, alpha, iterations):
-    W1, b1, W2, b2 = init_params()
-    for i in range(iterations):
-        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
-        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-        if i % 10 == 0:
-            print("Iteration: ", i)
-            predictions = get_predictions(A2)
-            print(get_accuracy(predictions, Y))
-    return W1, b1, W2, b2
-
-print(X_train.shape)
-print(y_train.shape)
-print(one_hot(y_train).shape)
-W1, b1, W2, b2 = gradient_descent(X_train, y_train, 0.10, 5000)
+if __name__ == "__main__":
+    main()
