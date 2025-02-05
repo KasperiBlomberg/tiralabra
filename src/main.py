f640318a64f82a96a856abe6b2666b27be4c82a7
KasperiBlomberg <blomberg.kasperi@gmail.com>
2025-02-05 20:15:49 +0200

    Improve UI

--- src/main.py
+++ src/main.py
@@ -10,11 +10,17 @@ def main():
 
         if choice == "1":
             sample_size = int(input("Enter the sample size: (max 60000)"))
+            if sample_size > 60000:
+                print("Sample size cannot be greater than 60000. Setting it to 60000.")
+                sample_size = 60000
             iterations = int(input("Enter the number of iterations: "))
             alpha = float(input("Enter the learning rate: "))
             train(alpha, iterations, sample_size)
         elif choice == "2":
             sample_size = int(input("Enter the sample size: (max 10000)"))
+            if sample_size > 10000:
+                print("Sample size cannot be greater than 10000. Setting it to 10000.")
+                sample_size = 10000
             evaluate(sample_size)
         elif choice == "3":
             break
